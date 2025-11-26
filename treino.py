fcc344b6515192663b4fe1b83916ba3c70a2e35d
SAbatistadrg <igor.batista@sandech.com.br>
2025-11-26 13:44:55 -0300

    Com val

--- treino.py
+++ treino.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 1. Carrega o modelo base (pré-treinado)
-model = YOLO("runs/detect/train4/weights/best.pt")  # versão pequena e rápida
+model = YOLO("yolov8n.pt")  # versão pequena e rápida
 
 # 2. Treina o modelo usando seu dataset
 model.train(
@@ -9,5 +9,5 @@ model.train(
     epochs=50,                  # pode alterar depois
     imgsz=640,                  # tamanho das imagens
     batch=8,                    # ajuste se der erro de RAM
-    device="cpu"                    # 0 = GPU, "cpu" se não tiver GPU
+    device="0"                    # 0 = GPU, "cpu" se não tiver GPU
 )
